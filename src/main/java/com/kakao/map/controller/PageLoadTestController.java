88a07e992f25ac9ac0b643a0efab3a6930d519fc
ornornro <roornorn@gmail.com>
2022-11-28 21:52:56 +0900

    카카오맵 연결

--- src/main/java/com/kakao/map/controller/PageLoadTestController.java
+++ src/main/java/com/kakao/map/controller/PageLoadTestController.java
@@ -9,6 +9,19 @@ public class PageLoadTestController {
 
     @RequestMapping(value = "/t1", method = RequestMethod.GET)
     public String test1() {
+        System.out.println("t1 요청 들어옴?");
         return "test1";
     }
+
+    @RequestMapping(value = "/test/page", method = RequestMethod.GET)
+    public String test2() {
+        System.out.println("t2 요청 들어옴?");
+        return "test/test2";
+    }
+
+    @RequestMapping(value = "/map", method = RequestMethod.GET)
+    public String kakaoMap() {
+        return "map";
+    }
+
 }
